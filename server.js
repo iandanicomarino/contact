27deb4edabdaad24cd2881e04adf95cd2f5f49bd
nekomarino <nekomarino@gmail.com>
2016-03-30 12:48:38 +0800

    connected to node server

--- server.js
+++ server.js
@@ -2,5 +2,34 @@ var express = require ('express');
 var app = express();
 
 app.use(express.static(__dirname+"/public"));
+
+//app.get is processing $http.get() from controller.js
+app.get('/contactlist', function (req,res){
+	console.log("i received a get");
+	
+	person1 ={
+		name:"ian marino",
+		email:"nekomarino",
+		number: "111-1111"
+	};
+	person2 ={
+		name:"ian marino",
+		email:"nekomarino",
+		number: "111-1111"
+	};
+	person3 ={
+		name:"ian marino",
+		email:"nekomarino",
+		number: "222-1111"
+	};
+
+	//console.log(person1);
+	var contactlist = [];
+	contactlist.push(person1);
+	contactlist.push(person2);
+	contactlist.push(person3);
+	//console.log(contactlist);
+	res.json(contactlist);//send back data to controller as respond
+});
 app.listen(1234);
 console.log("server running");
